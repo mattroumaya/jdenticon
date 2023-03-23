ad254bff941edf3b7698eb0e53a6296699b97408
mattroumaya <matthewroumaya@gmail.com>
2023-03-23 09:07:41 -0400

    init

---
+++ R/jdenticon.R
@@ -0,0 +1,61 @@
+#' Create a Jdenticon.
+#'
+#' @param value `character` Value to be converted to hexadecimal hash to render Jdenticon.
+#' @param filePath `character` File path to save Jdenticon .png to. If `NULL`, defaults to current working directory.
+#' @param fileName `character` File name to save Jdenticon .png as. If `NULL`, defaults to `temp_jdenticon_{value}`.
+#' @param size `numeric` Size of Jdenticon. Default == 100.
+#' @param preview `boolean` Preview Jdenticon in viewer pane?
+#'
+#'
+#' @examples
+#' \dontrun{
+#' jdenticon(value = 'mango')
+#' }
+#'
+#' @return Jdenticon Icon.
+#'
+#'
+#' @export
+jdenticon <- function(
+    value = NULL,
+    filePath = NULL,
+    fileName = NULL,
+    size = "100",
+    preview = TRUE)
+  {
+
+  if (is.null(value)) {
+    value <- rawToChar(as.raw(sample(c(65:90,97:122), 9, replace=T)))
+  }
+
+  if (is.null(filePath)) {
+    filePath <- fs::path_abs(getwd())
+  } else {
+    # filePath arg
+    filePath <- fs::path_abs(outfile)
+  }
+
+  if (is.null(fileName)) {
+    fileName <- paste0("temp_jdenticon_", value)
+  }
+
+  console.log <- processx::run(
+    command = "node",
+    args = c(
+      "script/index.js",
+      filePath,
+      fileName,
+      size,
+      value),
+    wd = system.file("node", package = "jdenticon")
+  )[["stdout"]]
+
+  if (preview) {
+    print(magick::image_read(paste0(filePath, "/", fileName, ".png")))
+  }
+
+  return(
+    glue::glue_collapse(console.log, sep = "\n")
+  )
+
+}
---
+++ R/jdenticon_npm_install.R
@@ -0,0 +1,32 @@
+#' Install jdenticon npm dependency.
+#'
+#' @param force `boolean` If `TRUE`, automatically proceeds with npm installation. If `FALSE` (default), prompts user before proceeding with npm installation.
+#'
+#' @return Updated `inst/node_modules` folder.
+#'
+#' @examples
+#' \dontrun{
+#' jdenticon_npm_install(force = TRUE)
+#' }
+#'
+#' @export
+jdenticon_npm_install <- function(
+    force = FALSE
+){
+  # Prompt the users unless they bypass (we're installing stuff on their machine)
+  if (!force) {
+    ok <- yesno::yesno("This will install the jdenticon library on your local directory. Ok to proceed? ")
+  } else {
+    ok <- TRUE
+  }
+
+  # If user is ok, run npm install in the node folder in the package folder
+  # We should also check that the infra is not already there
+  if (ok){
+    processx::run(
+      command = "npm",
+      args = c("install"),
+      wd = system.file("node", package = "jdenticon")
+    )
+  }
+}
