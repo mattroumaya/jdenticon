725659e944fff3788cb70c57b9623d5330181a13
Richard Morey <richarddmorey@gmail.com>
2023-03-29 14:57:34 +0100

    Revert "Enable all jdenticon options (including config and svg)"
    This reverts commit cc967f6be5ef49aa567f9ff96c9108a67cf15d1e.

--- R/jdenticon.R
+++ R/jdenticon.R
@@ -4,6 +4,8 @@
 #' @param filePath `character` File path to save Jdenticon .png to. If `NULL`, defaults to current working directory.
 #' @param fileName `character` File name to save Jdenticon .png as. If `NULL`, defaults to `temp_jdenticon_{value}`.
 #' @param size `numeric` Size of Jdenticon. Default == 100.
+#' @param config list of jdenticon configuration options (see [the jdenticon documentation](https://jdenticon.com/js-api/M_jdenticon_toPng.html))
+#' @param type Image type (default 'png', or 'svg')
 #' @param preview `boolean` Preview Jdenticon in viewer pane?
 #'
 #'
@@ -23,14 +25,19 @@
 #' @export
 jdenticon <- function(
     value = NULL,
-    filePath = NULL,
-    fileName = NULL,
+    filePath = tempdir(),
+    fileName = glue::glue('jdenticon_{size}_{value}'),
     size = "100",
-    preview = TRUE)
+    config = NULL,
+    type = 'png',
+    preview = interactive() && Sys.getenv("RSTUDIO") == "1")
   {
 
+  if(!(type %in% c('png','svg')) )
+    stop('Argument type must be one of "png" or "svg".')
+
   if (is.null(value)) {
-    value <- rawToChar(as.raw(sample(c(65:90,97:122), 9, replace=T)))
+    value <- rawToChar(as.raw(sample(c(65:90,97:122), 9, replace=TRUE)))
   }
 
   if (is.null(filePath)) {
@@ -44,6 +51,8 @@ jdenticon <- function(
     fileName <- paste0("temp_jdenticon_", value)
   }
 
+  config_json <- jsonlite::toJSON(config, auto_unbox = TRUE) |> as.character()
+
   console.log <- processx::run(
     command = "node",
     args = c(
@@ -51,12 +60,14 @@ jdenticon <- function(
       filePath,
       fileName,
       size,
-      value),
+      value,
+      config_json,
+      type),
     wd = system.file("node", package = "jdenticon")
   )[["stdout"]]
 
   if (preview) {
-    print(magick::image_read(paste0(filePath, "/", fileName, ".png")))
+    print(magick::image_read(glue::glue('{filePath}/{fileName}.{type}')))
   }
 
   return(
--- inst/node/index.js
+++ inst/node/index.js
@@ -7,28 +7,17 @@ const filePath = process.argv[2];
 const fileName = process.argv[3];
 const size = Number(process.argv[4]);
 const value = process.argv[5];
-const config = process.argv[6];
-const type = process.argv[7];
-const config_json = JSON.parse(config);
-
-var fname = '';
 
 // create jdenticon
-if(type === 'svg'){
-  fname = jdenticon.toSvg(value, size, config_json);
-}else{
-  fname = jdenticon.toPng(value, size, config_json);
-}
+const png = jdenticon.toPng(value, size);
 
 // log
-console.log(`--- Creating jdenticon of type      : ${type}`);
-console.log(`--- Creating jdenticon using value  : ${value}`);
-console.log(`--- Creating jdenticon using size   : ${size}`);
-console.log(`--- Creating jdenticon using options: ${config}`);
-console.log(`--- Saving jdenticon to path        : ${filePath}`);
+console.log(`--- Creating jdenticon using value: ${value}`);
+console.log(`--- Creating jdenticon using size: ${size}`);
+console.log(`--- Saving jdenticon to path: ${filePath}`);
 
 // save
-fs.writeFileSync(`${filePath}/${fileName}.${type}`, fname);
+fs.writeFileSync(`${filePath}/${fileName}.png`, png);
 
 
 
